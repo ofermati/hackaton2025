e10c3a0595d94983b3a18406aca4a38a959677fe
ofer <ofermati@post.bgu.ac.il>
2025-04-03 13:12:27 +0300

    Refactor heart rate simulation and add status JSON file

--- pulsData.py
+++ pulsData.py
@@ -3,9 +3,14 @@ import time
 import random
 
 while True:
-    # מדמה דופק (אולי פתאום התקף חרדה)
-    heart_rate = random.randint(108, 130)
     attack_detected = heart_rate > 110
+    # מדמה דופק (אולי פתאום התקף חרדה)
+    for i in range (10):
+        heart_rate = random.randint(60, 80)  # דופק בין 60 ל-80
+        time.sleep(0.5)
+    for i in range (10):
+        heart_rate = random.randint(115, 130)
+        time.sleep(0.5)
 
     # יוצרים מבנה נתונים
     data = {
@@ -19,4 +24,4 @@ while True:
         json.dump(data, f, ensure_ascii=False, indent=2)
 
     print(f"שודר דופק: {heart_rate} | התקף? {attack_detected}")
-    time.sleep(3)  # פולס כל 3 שניות
+    time.sleep(1)  # פולס כל 3 שניות
